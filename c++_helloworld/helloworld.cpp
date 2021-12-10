20f853cf903efa14012df73f52f4c368a4701547
konguele <juanjo.gmanzano@gmail.com>
2021-12-10 10:37:04 +0100

    hello world

---
+++ c++_helloworld/helloworld.cpp
@@ -0,0 +1,13 @@
+//Primer programa c++
+
+#include<iostream> //libreria standard
+
+using namespace std; //sirve para imprimir algo en pantalla
+
+int main (){ //Función principal
+
+    cout<<"Hola mundo :)"; //Todo lo que haya dentro de cout lo imprime por pantalla
+
+
+    return 0; //le tenemos que dar un return
+}
